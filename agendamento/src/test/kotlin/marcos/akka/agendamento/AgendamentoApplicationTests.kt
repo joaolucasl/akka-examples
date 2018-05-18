1f53c49282e09a394fdcb3df30b80b3aac843480
Marcos Arruda <marcos.f.arruda@gmail.com>
2018-05-18 00:37:01 -0300

    adding a bunch of scripts

--- agendamento/src/test/kotlin/marcos/akka/agendamento/AgendamentoApplicationTests.kt
+++ agendamento/src/test/kotlin/marcos/akka/agendamento/AgendamentoApplicationTests.kt
@@ -1,5 +1,6 @@
-package marcos.akka.atorlocal
+package marcos.akka.agendamento
 
+import marcos.akka.agendamento.AgendamentoApplication
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
@@ -11,6 +12,7 @@ class AgendamentoApplicationTests {
 
 	@Test
 	fun contextLoads() {
+        println("Porra")
 	}
 
 }
