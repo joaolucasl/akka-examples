06b6de8bb24c5a57a784baaed440582e48adfed8
Marcos Arruda <marcos.f.arruda@gmail.com>
2018-05-17 23:50:22 -0300

    first commit

---
+++ ator-local/src/main/kotlin/marcos/akka/atorlocal/AtorLocalApplication.kt
@@ -0,0 +1,11 @@
+package marcos.akka.atorlocal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtorLocalApplication
+
+fun main(args: Array<String>) {
+    runApplication<AtorLocalApplication>(*args)
+}
---
+++ sharding/src/test/kotlin/marcos/akka/sharding/ShardingApplicationTests.kt
@@ -0,0 +1,16 @@
+package marcos.akka.sharding
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class ShardingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
