e2e66070d20d39f1d285727625ab9ee2f9cf4ddb
Marcos Arruda <marcos.f.arruda@gmail.com>
2018-05-19 20:22:03 -0300

    adding modularity

---
+++ ator-local/src/main/kotlin/marcos/akka/config/AtorLocalProducer.kt
@@ -0,0 +1,15 @@
+package marcos.akka.config
+
+import akka.actor.Actor
+import akka.actor.IndirectActorProducer
+import org.springframework.context.ApplicationContext
+
+class AtorLocalProducer(private val applicationContext: ApplicationContext, private val beanActorName: String, private val specificName: String) : IndirectActorProducer {
+    override fun produce(): Actor {
+        return applicationContext.getBean(beanActorName, specificName) as Actor
+    }
+
+    override fun actorClass(): Class<out Actor>? {
+        return applicationContext.getType(beanActorName) as Class<out Actor>?
+    }
+}
---
+++ common-model/src/main/kotlin/marcos/akka/configs/ActorSystemConfigurationExample.kt
@@ -0,0 +1,15 @@
+package marcos.akka.configs
+
+//@Configuration
+class ActorSystemConfigurationExample {
+
+    //@Autowired
+    //private val springExtension: SpringExtension? = null
+
+    //@Bean
+    //fun actorSystem(): ActorSystem {
+    //    val system = ActorSystem.create("akka-spring-moip")
+    //    springExtension!!.get(system)
+    //    return system
+    //}
+}
