54a3a6daf3cbcb8603e79046ff037ed638f481b6
Marcos Arruda <marcos.f.arruda@gmail.com>
2018-06-05 12:56:18 -0300

    improving manager.sh with multiple docker-compose.yml files

--- manager.sh
+++ manager.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
+ENV_TYPES=("generic" "clustering" "sharding")
+
+function echoCurrentEnv(){
+  echo "CURRENT_ENV = $(cat CURRENT_ENV)"
+}
+
 function prune(){
-    docker system prune -f
-    docker volume prune -f
+    docker system prune -f 1> /dev/null
+    docker volume prune -f 1> /dev/null
 }
 
 function stopAll(){
-    docker-compose down
+    docker-compose -f compose-$(cat CURRENT_ENV).yml down
 }
 
 function buildAll(){
@@ -14,16 +20,54 @@ function buildAll(){
 }
 
 function upAll(){
-    docker-compose up -d
+    echoCurrentEnv
+    docker-compose -f compose-$(cat CURRENT_ENV).yml up -d
+}
+
+function printUsage(){
+  echo './manager.sh env generic|clustering|sharding    #change env'
+  echo './manager.sh reset                              #stop all containers and reset env to generic'
+  echo './manager.sh stop                               #stop all containers'
+  echo './manager.sh rebuild-start                      #rebuild and restart all containers'
+  echo './manager.sh rebuild                            #rebuild all containers'
+  echo './manager.sh start                              #start all containers'
+  echo './manager.sh status                             #show all containers status'
+  echo ''
+  echo "CURRENT_ENV = $(cat CURRENT_ENV)"
 }
 
 function dockerps(){
     docker ps -a
 }
 
-if [ "$1" = "stop" ]; then
+function containsElement(){
+    local e match="$1"
+    shift
+    for e; do [[ "$e" == "$match" ]] && return 0; done
+    return 1
+}
+
+function changeEnv(){
+  containsElement "$1" "${ENV_TYPES[@]}"
+  if [ "$?" = "0" ];then
+      echo "$1" > CURRENT_ENV
+      echoCurrentEnv
+      stopAll
+  fi
+}
+
+if [ $# -gt 1 ] && [ "$1" != "" ] && [ "$1" = "env" ] && [ "$2" != "" ]; then
+    changeEnv $2
+elif [ "$1" = "reset" ]; then
     stopAll
-    exit 0
+    prune
+    echo "generic" > CURRENT_ENV
+    echoCurrentEnv
+elif [ "$1" = "" ]; then
+    printUsage
+elif [ "$1" = "stop" ]; then
+    stopAll
+    prune
 elif [ "$1" = "rebuild-start" ]; then
     stopAll
     buildAll
@@ -37,8 +81,7 @@ elif [ "$1" = "start" ]; then
     prune
     upAll
 elif [ "$1" = "status" ]; then
-    dockerps
     prune
+    echoCurrentEnv
+    dockerps
 fi
-
-
